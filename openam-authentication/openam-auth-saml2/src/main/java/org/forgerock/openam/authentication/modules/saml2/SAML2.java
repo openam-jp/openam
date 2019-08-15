3856cf2ea26a9576d137cd519dc6fb59a74aca29
Yausshi Iwakata <iwakata@osstech.co.jp>
2019-08-15 12:11:48 +0900

    Added a copyright notice to the source code header.

--- openam-authentication/openam-auth-saml2/src/main/java/org/forgerock/openam/authentication/modules/saml2/SAML2.java
+++ openam-authentication/openam-auth-saml2/src/main/java/org/forgerock/openam/authentication/modules/saml2/SAML2.java
@@ -12,6 +12,7 @@
  * information: "Portions copyright [year] [name of copyright owner]".
  *
  * Copyright 2015-2016 ForgeRock AS.
+ * Portions copyright 2019 Open Source Solution Technology Corporation
  */
 package org.forgerock.openam.authentication.modules.saml2;
 
