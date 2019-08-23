fc9beac025ea8387c18bcc4b129b243ff2db5e06
TSUJIGUCHI Takaya <tsujiguchi@osstech.co.jp>
2019-08-23 11:08:37 +0900

    Resolve ESLint error in webauthn.js

--- openam-ui/openam-ui-ria/src/main/js/jp/co/osstech/openam/server/util/webauthn.js
+++ openam-ui/openam-ui-ria/src/main/js/jp/co/osstech/openam/server/util/webauthn.js
@@ -1,78 +1,65 @@
 define("jp/co/osstech/openam/server/util/webauthn", [
-	"jquery"
-],	function ($) {
-	
-	var obj = {};
-	
-	obj.createCredential = function (options){
-		$('#idToken4_0').hide();
-		navigator.credentials.create(options)
-		
-		.then((Credential) => {
-			
-			let attestationObject = new Uint8Array(Credential.response.attestationObject);
-			let clientDataJSON = new Uint8Array(Credential.response.clientDataJSON);
-			let rawId = new Uint8Array(Credential.rawId);
-			
-			const jdata = {
-					id: Credential.id,
-					rawId: btoa(String.fromCharCode.apply(null, rawId)),
-					type: Credential.type,
-					attestationObject: btoa(String.fromCharCode.apply(null, attestationObject)),
-					clientDataJSON: btoa(String.fromCharCode.apply(null, clientDataJSON))
-			};
+    "jquery"
+], function ($) {
 
-			const jsonbody = JSON.stringify(jdata);
-			
-			console.log("NEW CREDENTIAL", Credential);
-			console.log("Json data", jsonbody);
-			$('#PublicKeyCredential').val(jsonbody);
-			$('#idToken4_0').click();
+    var obj = {};
 
-		})
-	    
-		.catch((err) => {
-			console.log("ERROR", err);
-		});
-	};
-	
-	obj.getCredential = function (options){
-		$('#idToken4_0').hide();
-		navigator.credentials.get(options)
-		
-		.then((Credential) => {
-			
-			let clientDataJSON = new Uint8Array(Credential.response.clientDataJSON);
-			let signature = new Uint8Array(Credential.response.signature);
-			let authenticatorData = new Uint8Array(Credential.response.authenticatorData);
-			let rawId = new Uint8Array(Credential.rawId);
-			let userHandle = new Uint8Array(Credential.response.userHandle);
-			
-			const jdata = {
-					id: Credential.id,
-					rawId: btoa(String.fromCharCode.apply(null, rawId)),
-					userHandle: btoa(String.fromCharCode.apply(null, userHandle)),
-					authenticatorData: btoa(String.fromCharCode.apply(null, authenticatorData)),
-					signature: btoa(String.fromCharCode.apply(null, signature)),
-					clientDataJSON: btoa(String.fromCharCode.apply(null, clientDataJSON))
-			};
+    obj.createCredential = function (options) {
+        $("#idToken4_0").hide();
+        navigator.credentials.create(options)
+            .then((Credential) => {
+                const attestationObject = new Uint8Array(Credential.response.attestationObject);
+                const clientDataJSON = new Uint8Array(Credential.response.clientDataJSON);
+                const rawId = new Uint8Array(Credential.rawId);
 
-			const jsonbody = JSON.stringify(jdata);
-			
-			console.log("Assersion", Credential);
-			console.log("Json data", jsonbody);
-			$('#PublicKeyCredential').val(jsonbody);
-			$('#idToken4_0').click();
+                const jdata = {
+                    id: Credential.id,
+                    rawId: btoa(String.fromCharCode.apply(null, rawId)),
+                    type: Credential.type,
+                    attestationObject: btoa(String.fromCharCode.apply(null, attestationObject)),
+                    clientDataJSON: btoa(String.fromCharCode.apply(null, clientDataJSON))
+                };
 
+                const jsonbody = JSON.stringify(jdata);
+                console.log("NEW CREDENTIAL", Credential);
+                console.log("Json data", jsonbody);
+                $("#PublicKeyCredential").val(jsonbody);
+                $("#idToken4_0").click();
+            })
+            .catch((err) => {
+                console.log("ERROR", err);
+            });
+    };
 
-		})
-			
-		.catch((err) => {
-			console.log("ERROR", err);
-		});
-		
-	};
+    obj.getCredential = function (options) {
+        $("#idToken4_0").hide();
+        navigator.credentials.get(options)
+            .then((Credential) => {
+                const clientDataJSON = new Uint8Array(Credential.response.clientDataJSON);
+                const signature = new Uint8Array(Credential.response.signature);
+                const authenticatorData = new Uint8Array(Credential.response.authenticatorData);
+                const rawId = new Uint8Array(Credential.rawId);
+                const userHandle = new Uint8Array(Credential.response.userHandle);
 
-	return obj;
-	
+                const jdata = {
+                    id: Credential.id,
+                    rawId: btoa(String.fromCharCode.apply(null, rawId)),
+                    userHandle: btoa(String.fromCharCode.apply(null, userHandle)),
+                    authenticatorData: btoa(String.fromCharCode.apply(null, authenticatorData)),
+                    signature: btoa(String.fromCharCode.apply(null, signature)),
+                    clientDataJSON: btoa(String.fromCharCode.apply(null, clientDataJSON))
+                };
+
+                const jsonbody = JSON.stringify(jdata);
+                console.log("Assersion", Credential);
+                console.log("Json data", jsonbody);
+                $("#PublicKeyCredential").val(jsonbody);
+                $("#idToken4_0").click();
+            })
+            .catch((err) => {
+                console.log("ERROR", err);
+            });
+    };
+
+    return obj;
 });
