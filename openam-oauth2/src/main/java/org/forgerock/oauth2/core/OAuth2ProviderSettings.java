371d28f2acae9849631ae48b5be7b1e75dfe5cd6
Kurotsu <Kurotsu@10.0.1.105>
2019-09-20 11:53:06 +0900

    Add copyright

--- openam-oauth2/src/main/java/org/forgerock/oauth2/core/OAuth2ProviderSettings.java
+++ openam-oauth2/src/main/java/org/forgerock/oauth2/core/OAuth2ProviderSettings.java
@@ -13,6 +13,7 @@
  *
  * Copyright 2014-2016 ForgeRock AS.
  * Portions Copyrighted 2015 Nomura Research Institute, Ltd.
+ * Portions copyright 2019 Open Source Solution Technology Corporation
  */
 
 package org.forgerock.oauth2.core;
